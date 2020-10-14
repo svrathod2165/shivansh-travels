d77367482351fc40529a10839fc360da6bc853e0
svrathod2165 <54907876+svrathod2165@users.noreply.github.com>
2020-10-15 03:03:55 +0530

    upperheader

--- src/components/UpperHeader/UpperHeader.js
+++ src/components/UpperHeader/UpperHeader.js
@@ -12,8 +12,14 @@ const EmailUpper = styled.div`
   padding: 14px 10px 14px 2px;
 `;
 
-const EmailIcon = styled.img`
-  padding-right: 5px;
+const PhoneUpper = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 21px;
+  color: #000;
+  display: inline-block;
+  padding: 14px 10px 14px 2px;
 `;
 
 function UpperHeader() {
@@ -22,16 +28,21 @@ function UpperHeader() {
       <div className="upper-header">
         <div className="upper-header-inner-box">
           <EmailUpper>
-            <EmailIcon>
-              <img
-                src="https://saibabatravels.com/wp-content/themes/saibaba/images/black-mail-icon.svg"
-                alt="email"
-              />
-            </EmailIcon>
+            <img
+              className="email-icon"
+              src="https://saibabatravels.com/wp-content/themes/saibaba/images/black-mail-icon.svg"
+              alt="email"
+            />
             info@saibabatravels.com
           </EmailUpper>
-          {/* <div className="email-upper">info@saibabatravels.com</div> */}
-          <div className="phone-up">Call Us: +91 98231 67777</div>
+          <PhoneUpper>
+            <img
+              className="email-icon"
+              src="https://saibabatravels.com/wp-content/themes/saibaba/images/call-icon.svg"
+              alt="email"
+            />
+            Call Us: +91 98231 67777
+          </PhoneUpper>
         </div>
       </div>
     </>
