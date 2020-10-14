39b9ab9d13a6abe4b8f5bdd053d449f04f760b3c
svrathod2165 <54907876+svrathod2165@users.noreply.github.com>
2020-10-14 14:54:49 +0530

    ch1

---
+++ src/components/UpperHeader/UpperHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./UpperHeader.css";
+
+function UpperHeader() {
+  return (
+    <>
+      <div className="upper-header">
+        <div className="email-upper">info@saibabatravels.com</div>
+        <div className="phone-up">Call Us: +91 98231 67777</div>
+      </div>
+    </>
+  );
+}
+
+export default UpperHeader;
